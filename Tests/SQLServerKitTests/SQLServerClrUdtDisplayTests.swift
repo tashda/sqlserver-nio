d99540d90faf1fbe99a9b1d935129a0f7399d08e
tashda <kenneth@skabel.se>
2026-03-20 10:20:08 +0100

    feat: render hierarchyid as canonical path

--- Tests/SQLServerKitTests/SQLServerClrUdtDisplayTests.swift
+++ Tests/SQLServerKitTests/SQLServerClrUdtDisplayTests.swift
@@ -4,7 +4,46 @@ import NIOCore
 @testable import SQLServerTDS
 
 final class SQLServerClrUdtDisplayTests: XCTestCase, @unchecked Sendable {
-    func testHierarchyIdDescriptionRendersHexValue() {
+    func testHierarchyIdDescriptionRendersCanonicalPath() {
+        var buffer = ByteBufferAllocator().buffer(capacity: 3)
+        buffer.writeBytes([0x7C, 0x2B, 0x78])
+
+        let value = SQLServerValue(base: TDSData(metadata: hierarchyIDMetadata, value: buffer))
+
+        XCTAssertEqual(value.udtTypeName, "hierarchyid")
+        XCTAssertEqual(value.description, "/3/4/1/3/")
+    }
+
+    func testHierarchyIdDescriptionRendersAdventureWorksPath() {
+        var buffer = ByteBufferAllocator().buffer(capacity: 2)
+        buffer.writeBytes([0x5A, 0xDE])
+
+        let value = SQLServerValue(base: TDSData(metadata: hierarchyIDMetadata, value: buffer))
+
+        XCTAssertEqual(value.description, "/1/1/3/")
+    }
+
+    func testHierarchyIdDescriptionRendersDottedNegativePath() {
+        var buffer = ByteBufferAllocator().buffer(capacity: 3)
+        buffer.writeBytes([0x54, 0x07, 0x30])
+
+        let value = SQLServerValue(base: TDSData(metadata: hierarchyIDMetadata, value: buffer))
+
+        XCTAssertEqual(value.description, "/0.3.-7/")
+    }
+
+    func testHierarchyIdDescriptionRendersRootForEmptyPayload() {
+        let value = SQLServerValue(
+            base: TDSData(
+                metadata: hierarchyIDMetadata,
+                value: ByteBufferAllocator().buffer(capacity: 0)
+            )
+        )
+
+        XCTAssertEqual(value.description, "/")
+    }
+
+    func testNonHierarchyClrUdtStillRendersHex() {
         var buffer = ByteBufferAllocator().buffer(capacity: 3)
         buffer.writeBytes([0x7C, 0x2B, 0x78])
 
@@ -16,18 +55,34 @@ final class SQLServerClrUdtDisplayTests: XCTestCase, @unchecked Sendable {
             precision: 0,
             scale: 0,
             collation: [],
-            colName: "OrganizationNode",
+            colName: "SomeUdt",
             udtInfo: .init(
                 databaseName: "sys",
                 schemaName: "dbo",
-                typeName: "hierarchyid",
+                typeName: "geography",
                 assemblyName: "Microsoft.SqlServer.Types"
             )
         )
 
         let value = SQLServerValue(base: TDSData(metadata: metadata, value: buffer))
 
-        XCTAssertEqual(value.udtTypeName, "hierarchyid")
         XCTAssertEqual(value.description, "0x7C2B78")
     }
 }
+
+private let hierarchyIDMetadata = TDSTokens.ColMetadataToken.ColumnData(
+    userType: 0,
+    flags: 0,
+    dataType: .clrUdt,
+    length: 128,
+    precision: 0,
+    scale: 0,
+    collation: [],
+    colName: "OrganizationNode",
+    udtInfo: .init(
+        databaseName: "sys",
+        schemaName: "dbo",
+        typeName: "hierarchyid",
+        assemblyName: "Microsoft.SqlServer.Types"
+    )
+)
